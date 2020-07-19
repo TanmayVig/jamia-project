45b3e2384e582e26964f2e0a154efede4f670223
Shinda <tanmayvij11@gamil.com>
2020-07-20 00:16:36 +0530

    footer added, index page decorated

--- KnowYourJMI/KnowYourJMI/wsgi.py
+++ KnowYourJMI/KnowYourJMI/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
+# added by me
+from django.conf import settings
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+# till here
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'KnowYourJMI.settings')
 
-application = get_wsgi_application()
+if settings.DEBUG:
+    application=StaticFilesHandler(get_wsgi_application())
+else:
+    application = get_wsgi_application()
